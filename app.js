f67e529912196e43cde98c5efec000a2a5b90c0b
Ashok R M <31401266+AshokRamanathan12@users.noreply.github.com>
2024-11-14 15:41:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-var http = require('http');
-var fs = require('fs');
+// src/components/EnvironmentCard.js
+import React, { useState } from 'react';
+import { Box, ListItem, ListItemText, Collapse, Grid, Card, CardContent, Typography } from '@mui/material';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
-fs.readFile('./index.html', function (err, data) {
-    if (err) {
-        console.log(err);
-    }
-    var server = http.createServer(function (req, res) {   //create web server
-        if (req.url == '/memorycard/play') { //check the URL of the current request
+const EnvironmentCard = ({ env }) => {
+  const [open, setOpen] = useState(true);
 
-            // set response header
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+  const handleToggle = () => {
+    setOpen(!open);
+  };
 
-            // set response content    
-            res.write(data);
-            res.end();
+  return (
+    <Box>
+      <ListItem button onClick={handleToggle}>
+        <ListItemText primary={env.name} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <Grid container spacing={2} sx={{ p: 2 }}>
+          {env.servers.map((server, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+              <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 2 }}>
+                <img src={server.image} alt={server.type} style={{ width: '60px', height: '60px' }} />
+                <CardContent>
+                  <Typography variant="h6">{server.type}</Typography>
+                  <Typography variant="h4" color="primary" sx={{ fontWeight: 'bold' }}>
+                    {server.count}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Collapse>
+    </Box>
+  );
+};
 
-        }
-    }).listen(3000);
-})
-
-
+export default EnvironmentCard;
